1205afc8f83ce6c8779d07b2c7773c93e83967fb
nicholas-ngzy <3851655+nicholas-ngzy@users.noreply.github.com>
2024-04-14 10:47:02 +0800

    Update form to use react-hook-form, zod and shadcn

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,120 +1,134 @@
-import { InputAdornment, Stack, TextField } from '@mui/material';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
-const Form = (props) => {
-    const { initialAmount, finalAmount, duration, interestRate, additionalContribution } = props.info;
+const formSchema = z.object({
+    initialAmount: z.coerce
+        .number({
+            required_error: 'Initial amount is required',
+            invalid_type_error: 'Initial amount must be a number',
+        })
+        .gte(0, 'Initial amount must be a positive number'),
+    // finalAmount: z.coerce
+    //     .number({
+    //         required_error: 'Final amount is required',
+    //         invalid_type_error: 'Final amount must be a number',
+    //     })
+    //     .gt(0, 'Final amount must be more than 0'),
+    duration: z.coerce
+        .number({
+            required_error: 'Duration is required',
+            invalid_type_error: 'Duration must be a number',
+        })
+        .gte(1, 'Duration must be more than 0'),
+    interestRate: z.coerce
+        .number({
+            required_error: 'Interest rate is required',
+            invalid_type_error: 'Interest rate must be a number',
+        })
+        .gt(0, 'Interest rate must be more than 0'),
+    additionalContribution: z.coerce
+        .number({
+            required_error: 'Additional contribution is required',
+            invalid_type_error: 'Additional contribution must be a number',
+        })
+        .nonnegative('Additional contribution must be a positive number'),
+});
+
+const InputForm = (props) => {
     const goal = props.goal;
-
-    const handleForm = (event) => {
-        event.preventDefault();
-        props.updateForm(event.target.name, event.target.value);
-    };
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            initialAmount: 1000,
+            // finalAmount: 0,
+            duration: 1,
+            interestRate: 1,
+            additionalContribution: 0,
+        },
+    });
 
     return (
-        <Stack spacing={2}>
-            <TextField
-                id='initial-amount'
-                inputProps={{
-                    inputMode: 'decimal',
-                    pattern: '([0-9]+)?[,\\.]?[0-9]*',
-                    inputProps: {
-                        min: 0,
-                    },
-                }}
-                startAdornment={<InputAdornment position='start'>$</InputAdornment>}
-                InputProps={{
-                    startAdornment: <InputAdornment position='start'>$</InputAdornment>,
-                }}
-                label='Initial amount'
-                name='initialAmount'
-                onChange={handleForm}
-                required
-                type='number'
-                value={initialAmount}
-            />
-            {goal !== 'final-amount' ? (
-                <TextField
-                    id='final-amount'
-                    inputProps={{
-                        inputMode: 'decimal',
-                        pattern: '([0-9]+)?[,\\.]?[0-9]*',
-                        inputProps: {
-                            min: 0,
-                        },
-                    }}
-                    InputProps={{
-                        startAdornment: <InputAdornment position='start'>$</InputAdornment>,
-                    }}
-                    label='Final   amount'
-                    name='finalAmount'
-                    onChange={handleForm}
-                    required
-                    type='number'
-                    value={finalAmount}
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(props.handleSubmit)}>
+                <FormField
+                    control={form.control}
+                    name='initialAmount'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Initial Amount</FormLabel>
+                            <FormControl>
+                                <Input type='number' placeholder='1000' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-            ) : null}
-            {goal !== 'duration' ? (
-                <TextField
-                    id='duration'
-                    inputProps={{
-                        inputMode: 'decimal',
-                        pattern: '([0-9]+)?[,\\.]?[0-9]*',
-                        inputProps: {
-                            min: 0.01,
-                        },
-                    }}
-                    InputProps={{
-                        endAdornment: <InputAdornment position='end'>years</InputAdornment>,
-                    }}
-                    label='Duration'
+                {goal === 'final-amount' ? null : (
+                    <FormField
+                        control={form.control}
+                        name='finalAmount'
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Final Amount</FormLabel>
+                                <FormControl>
+                                    <Input placeholder='0' {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                )}
+                <FormField
+                    control={form.control}
                     name='duration'
-                    onChange={handleForm}
-                    required
-                    type='number'
-                    value={duration}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Duration</FormLabel>
+                            <FormControl>
+                                <Input placeholder='1' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-            ) : null}
-            <TextField
-                id='interest-rate'
-                inputProps={{
-                    inputMode: 'decimal',
-                    pattern: '([0-9]+)?[,\\.]?[0-9]*',
-                    inputProps: {
-                        min: 1,
-                    },
-                }}
-                InputProps={{
-                    endAdornment: <InputAdornment position='end'>%</InputAdornment>,
-                }}
-                label='Interest rate'
-                name='interestRate'
-                onChange={handleForm}
-                required
-                type='number'
-                value={interestRate}
-            />
-            {goal !== 'additional-contribution' ? (
-                <TextField
-                    id='additional-contribution'
-                    inputProps={{
-                        inputMode: 'decimal',
-                        pattern: '([0-9]+)?[,\\.]?[0-9]*',
-                        inputProps: {
-                            min: 0,
-                        },
-                    }}
-                    InputProps={{
-                        startAdornment: <InputAdornment position='start'>$</InputAdornment>,
-                    }}
-                    label='Additional annual contribution'
+                <FormField
+                    control={form.control}
+                    name='interestRate'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Interest rate</FormLabel>
+                            <FormControl>
+                                <Input placeholder='1' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
                     name='additionalContribution'
-                    onChange={handleForm}
-                    required
-                    type='number'
-                    value={additionalContribution}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Additional contribution</FormLabel>
+                            <FormControl>
+                                <Input placeholder='0' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-            ) : null}
-        </Stack>
+                <div className='flex items-center my-4'>
+                    <Button type='submit' className='flex-1'>
+                        Calculate
+                    </Button>
+                </div>
+            </form>
+        </Form>
     );
 };
 
-export default Form;
+export default InputForm;
